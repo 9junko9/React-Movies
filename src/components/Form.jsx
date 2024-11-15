c2650661866fe6953c642c4d095fed6ef19b3289
jennifer <jennifer.rivolier@gmail.com>
2024-11-15 17:56:05 +0100

    Add useEffect & useState form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,18 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import { useEffect } from "react";
 
 const Form = () => {
+  const [moviesData, setMoviesData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/search/movie?api_key=ed82f4c18f2964e75117c2dc65e2161d&query=code&language=fr-FR`
+      )
+      .then((res) => setMoviesData(res.data.results));
+  }, []);
+
   return (
     <div className="form-component">
       <div className="form-container">
@@ -21,7 +33,10 @@ const Form = () => {
           </div>
         </div>
       </div>
-      <div className="result">RESULTAT</div>
+      <div className="result"></div>
+      {moviesData.slice(0, 12).map((movie) => (
+        <h3>{movie.title}</h3>
+      ))}
     </div>
   );
 };
